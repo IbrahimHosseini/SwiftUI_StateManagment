8e05768fdee3356373c5692f402fb480a5eaa224
sehosseini@me.com <sehosseini@me.com>
2023-01-29 10:24:52 +0330

    Initial Commit

---
+++ SwiftUI_StateManagment/SwiftUI_StateManagmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StateManagmentApp.swift
+//  SwiftUI_StateManagment
+//
+//  Created by Ibrahim on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StateManagmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
